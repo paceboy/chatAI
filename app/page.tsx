ad74d8a7fb3b07485c078bd67ee7752eecaeab78
liushaogeng <liushaogeng@baidu.com>
2025-01-13 16:47:29 +0800

    解决link引入问题

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -48,10 +49,10 @@ export default function Home() {
           >
             Read our docs
           </a>
-          <a 
+          <Link 
           href="/chatAI">
             Goto ChatAI
-          </a>
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
